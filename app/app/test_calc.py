b54ea8565048ff507337c0a4fc67b616ceccc32d
Jason Lebreton <jlebreton@oxalia-software.com>
2019-03-19 12:41:51 +0100

    Add calc.subtract method + test.

--- app/app/test_calc.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-
-class CalcTests(TestCase):
-    def test_add_numbers(self):
-        """Add thow numers together"""
-        self.assertEqual(add(3, 8), 11)
